63783676e426d56276a3f38e3ea8891605fba1b9
Robin van Maasakker <robin@dashed.nl>
2026-07-30 16:39:17 +0200

    feat(livechat): visitor-blok in de app-conversatie-resource

---
+++ tests/Feature/ConversationResourceVisitorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Dashed\DashedLivechat\Tests\Support\Factories;
+use Dashed\DashedLivechat\Http\Resources\Api\Mobile\ConversationResource;
+
+it('bevat een visitor-blok met de metadata', function () {
+    $c = Factories::makeConversation([
+        'ip_hash' => 'HASH-X',
+        'visitor_ip' => '1.2.3.4',
+        'visitor_user_agent' => 'TestBrowser/1.0',
+        'visitor_referrer' => 'https://google.com/',
+        'visitor_city' => 'Amsterdam',
+        'visitor_country' => 'Netherlands',
+        'started_url' => 'https://shop.nl/product',
+    ]);
+    $arr = (new ConversationResource($c))->toArray(request());
+    expect($arr['visitor'])->toMatchArray([
+        'ip' => '1.2.3.4',
+        'user_agent' => 'TestBrowser/1.0',
+        'referrer' => 'https://google.com/',
+        'city' => 'Amsterdam',
+        'country' => 'Netherlands',
+        'started_url' => 'https://shop.nl/product',
+        'is_returning' => false,
+        'previous_count' => 0,
+    ]);
+});
